7bc62e71e1403202b83179912557472357f68dce
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2017-11-11 16:06:42 -0200

    Merge com o Guilherme

---
+++ CoinCoin/coincoinapp/www/js/bluetooth.js
@@ -0,0 +1,188 @@
+/**
+ * Created by Danila Loginov, December 23, 2016
+ * https://github.com/1oginov/Cordova-Bluetooth-Terminal
+ */
+
+'use strict';
+
+var app = {
+
+    initialize: function () {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady: function () {
+        app.goTo('paired-devices');
+
+        bluetoothSerial.isEnabled(app.listPairedDevices, function () {
+            app.showError('Enable bluetooth')
+        });
+
+        $('#refresh-paired-devices').click(app.listPairedDevices);
+        $('#paired-devices form').submit(app.selectDevice);
+        $('#toggle-connection').click(app.toggleConnection);
+        $('#clear-data').click(app.clearData);
+        $('#terminal form').submit(app.sendData);
+        $("#saldo").click(app.saldo);
+        $("#zerar").click(app.zerar);
+
+        $('#terminal .go-back').click(function () {
+            app.goTo('paired-devices');
+        });
+    },
+
+  
+    listPairedDevices: function () {
+        bluetoothSerial.list(function (devices) {
+            var $list = $('#paired-devices .list');
+
+            if (!devices.length) {
+                $list.text('Not found');
+                return;
+            }
+
+            $list.text('');
+            devices.forEach(function (device) {
+                $list.append('<label><input type="radio" name="device" value="' + device.address +
+                    '"><div><span class="name">' + device.name + '</span> <span class="address">' + device.address +
+                    '</span></div></label>');
+            });
+
+        }, app.showError);
+    },
+
+    selectDevice: function (event) {
+        event.preventDefault();
+
+        var $label = $('#paired-devices .list input[name=device]:checked').parent();
+
+        var name = $label.find('.name').text();
+        var address = $label.find('input').val();
+
+        if (!address) {
+            app.showError('Select paired device to connect');
+            return;
+        }
+
+        app.goTo('terminal');
+
+        var $selectedDevice = $('#selected-device');
+        $selectedDevice.find('.name').text(name);
+        $selectedDevice.find('.address').text(address);
+
+        app.connect(address);
+    },
+
+    toggleConnection: function () {
+        bluetoothSerial.isConnected(
+            // Disconnect if connected
+            function () {
+                bluetoothSerial.disconnect(app.deviceDisconnected, app.showError);
+            },
+
+            // Reconnect to selected device if disconnected
+            function () {
+                var address = $('#selected-device .address').text();
+
+                if (!address) {
+                    app.showError('Select paired device to connect');
+                    app.goTo('paired-devices');
+                    return;
+                }
+
+                app.connect(address);
+            }
+        );
+    },
+
+    connect: function (address) {
+        $('#selected-device .status').text('Connecting...');
+
+        // Attempt to connect device with specified address, call app.deviceConnected if success
+        bluetoothSerial.connect(address, app.deviceConnected, function (error) {
+            $('#selected-device .status').text('Disconnected');
+            app.showError(error);
+        });
+    },
+
+    deviceConnected: function () {
+        // Subscribe to data receiving as soon as the delimiter is read
+        bluetoothSerial.subscribe('\n', app.handleData, app.showError);
+
+        $('#selected-device .status').text('Connected');
+        $('#toggle-connection').text('Disconnect');
+    },
+
+    deviceDisconnected: function () {
+        // Unsubscribe from data receiving
+        bluetoothSerial.unsubscribe(app.handleData, app.showError);
+
+        $('#selected-device .status').text('Disconnected');
+        $('#toggle-connection').text('Connect');
+    },
+
+    handleData: function (data) {
+        app.displayInTerminal(data, true);
+    },
+
+    sendData: function (event) {
+        event.preventDefault();
+
+        var $input = $('#terminal form input[name=data]');
+        var data = $input.val();
+        $input.val('');
+
+       // data += '\n';
+
+        app.displayInTerminal(data, false);
+
+        bluetoothSerial.write(data, null, app.showError);
+    },
+
+    saldo: function (event) {
+        event.preventDefault();      
+        var data = 's';
+        app.displayInTerminal(data, false);
+        bluetoothSerial.write(data, null, app.showError);
+    },
+
+    zerar: function (event) {
+        event.preventDefault();
+        var data = 'l';
+        app.displayInTerminal(data, false);
+        bluetoothSerial.write(data, null, app.showError);
+    },
+
+    displayInTerminal: function (data, isIncoming) {
+        var $dataContainer = $('#data');
+
+        if (isIncoming) {
+            data = '<span class="in">' + data + '</span>';
+        }
+        else {
+            data = '<span class="out">' + data + '</span>';
+        }
+
+        $dataContainer.append(data);
+
+        if ($('#terminal input[name=autoscroll]').is(':checked')) {
+            $dataContainer.scrollTop($dataContainer[0].scrollHeight - $dataContainer.height());
+        }
+    },
+
+    clearData: function () {
+        $('#data').text('');
+    },
+
+    goTo: function (state) {
+        $('.state').hide();
+        $('#' + state + '.state').show();
+    },
+
+    showError: function (error) {
+        alert(error);
+    }
+
+};
+
+app.initialize();

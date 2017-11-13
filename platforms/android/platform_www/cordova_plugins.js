e4b4b83a85b32698a85efeafc4bd85687aaa7d8a
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2017-11-13 14:12:37 -0200

    Adicionando plugin cordova-sqlite-storage

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -7,13 +7,22 @@ module.exports = [
         "clobbers": [
             "window.bluetoothSerial"
         ]
+    },
+    {
+        "file": "plugins/cordova-sqlite-storage/www/SQLitePlugin.js",
+        "id": "cordova-sqlite-storage.SQLitePlugin",
+        "pluginId": "cordova-sqlite-storage",
+        "clobbers": [
+            "SQLitePlugin"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
     "cordova-plugin-whitelist": "1.3.2",
-    "cordova-plugin-bluetooth-serial": "0.4.5"
+    "cordova-plugin-bluetooth-serial": "0.4.5",
+    "cordova-sqlite-storage": "2.1.2"
 }
 // BOTTOM OF METADATA
 });

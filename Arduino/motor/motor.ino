59d6e57a0680975e34538aa94fddc3ff434b504b
André Vasconcelos <gp-andre.vasconcelos@uol.com.br>
2017-11-27 17:53:33 -0200

    Inserindo motor no projeto

---
+++ Arduino/motor/motor.ino
@@ -0,0 +1,38 @@
+#include <Servo.h>
+ 
+#define SERVO 6 // Porta Digital 6 PWM
+ 
+Servo s; // Variável Servo
+int pos; // Posição Servo
+ 
+void setup ()
+{
+  s.attach(SERVO);
+  Serial.begin(115200);
+  
+  s.write(0); // Inicia motor posição zero
+}
+ 
+void loop() {
+  abre();
+  delay(1000);
+  fecha();
+}
+
+  void abre(){
+    Serial.println("Abrindo...");
+    for(pos = 0; pos < 90; pos++) {
+      s.write(pos);
+      delay(15);
+    }
+  }
+
+  void fecha(){
+    Serial.println("Fechando...");
+    for(pos = 90; pos >= 0; pos--){
+      s.write(pos);
+      delay(15);
+    }
+  }
+
+
